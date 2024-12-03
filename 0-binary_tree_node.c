6bfb5d39054ba49005385c3a3d50eb9db7d1050b
OTI-OWOM <ezeogouba@gmail.com>
2024-12-03 07:33:27 -0800

    feat: create binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node and initializes it.
+ * @parent: Binary tree node's parent
+ * @value: Binary tree node's value
+ *
+ * Return: Pointer to the created binary tree node or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+
+return (new_node);
+}
+
